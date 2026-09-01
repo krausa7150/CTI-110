61048fc1d173016e69d38fd15d92ca93b52520f5
krausa7150 <krausa7150@student.faytechcc.edu>
2026-09-01 18:40:04 +0000

    P1Lab1 Finished

--- p1lab_Kraus.py
+++ p1lab_Kraus.py
@@ -6,3 +6,14 @@
 
 # First, greet the user!
 print("Hello Vietnaaaammmm User!!!!")
+
+#Next, ask the user for their name.
+#print("What is your name?")
+first_name = input("What is your name? ")
+
+# Then say back the name
+print(f"Nice to meet you, {first_name}!")
+
+#Say the same thing for last_name.
+last_name = input("What is your last name? ")
+print(f"Nice to meet you, {first_name} {last_name}!")
