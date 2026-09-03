a849b899135006225ba13b4157c93c0e6a532ccb
krausa7150 <krausa7150@student.faytechcc.edu>
2026-09-03 17:59:43 +0000

    p1Lab2_Kraus.py

---
+++ p1lab2_Kraus.py
@@ -0,0 +1,19 @@
+# CTI 110
+# P1LAB2
+# Kraus, A
+# 9/3/26  
+
+# Fictional Store -- Pick three things.
+# product_name, product_price, product_quantity.
+
+# Change these to your own values.
+product_name = "Jeep Badge"
+product_count = 1
+product_price = 19.99
+
+print ("Welcome to the", product_name, "store")
+print("We have", product_count, product_name, "(s) at $", product_price, "each.")
+
+total = product_count * product_price
+
+print("Your total is: $", total)
