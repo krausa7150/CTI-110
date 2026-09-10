1a61c8dd145e4d7915488fec55f59e784b849745
Alex Kraus <krausa7150@student.faytechcc.edu>
2026-09-10 18:24:22 +0000

    P1HW2_Kraus_Alex_Finished.py

--- P1HW2_Kraus_Alex.py
+++ P1HW2_Kraus_Alex.py
@@ -1,14 +1,25 @@
 # Alex Kraus
 # 09/10/26
 # P1HW2
-# Budgets
+# Travel Budgets
 
 print ("Enter budget:")
-budget = float(input())
+budget = int(input())
 print ("Enter your travel destination:")
 destination = input()
+print ("Approximately, how much will you need for fuel?")
+fuel_expenses = int(input())
 print ("Approximately, how much will you need for accommodations/hotel?")
-hotel_expenses = float(input())
+hotel_expenses = int(input())
 print ("Lastly, how much will you need for food?")
-food_expenses = float(input())
+food_expenses = int(input())
 print("-----Travel Expenses-----") 
+print("Location:", destination)
+print("Initial Budget:", "$" + str(budget))
+print("" * 30)
+print("Fuel:", "$" + str(fuel_expenses))
+print("Accommodation:", "$" + str(hotel_expenses))
+print("Food:", "$" + str(food_expenses)) 
+total_expenses = fuel_expenses + hotel_expenses + food_expenses
+print("" * 30)
+print("Remaining Balance:", "$" + str(budget - total_expenses)) 
