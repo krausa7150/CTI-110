66cc9cac9365d4ee839df5881e44bb2cb16e0a95
Alex Kraus <krausa7150@student.faytechcc.edu>
2026-09-10 18:20:01 +0000

    P1HW2_Kraus_Alex_Finished.py

---
+++ P1HW2_Kraus_Alex.py
@@ -0,0 +1,14 @@
+# Alex Kraus
+# 09/10/26
+# P1HW2
+# Budgets
+
+print ("Enter budget:")
+budget = float(input())
+print ("Enter your travel destination:")
+destination = input()
+print ("Approximately, how much will you need for accommodations/hotel?")
+hotel_expenses = float(input())
+print ("Lastly, how much will you need for food?")
+food_expenses = float(input())
+print("-----Travel Expenses-----") 
