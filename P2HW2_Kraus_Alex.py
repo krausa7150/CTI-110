f7e09ddf2fe3be6be155d0d3cb6350329bab9f18
Alex Kraus <krausa7150@student.faytechcc.edu>
2026-09-17 18:16:32 +0000

    P2HW_Kraus_Finished

---
+++ P2HW2_Kraus_Alex.py
@@ -0,0 +1,19 @@
+# Alex Kraus
+# CTI-110
+# 9/17/26
+# P2HW2
+# whattttt
+
+# This example only uses three numbers, full use six.
+# Get Grades
+grade1 = float(input("Enter grade #1"))
+grade2 = float(input("Enter grade #2"))
+grade3 = float(input("Enter grade #3"))
+
+
+
+
+# Do some calculations
+min_grade = min(grade_list)
+max_grade = max(grade_list)
+total
